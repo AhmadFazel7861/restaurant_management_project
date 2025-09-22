fa42d259ecce9b22ee88df60c1d96b5bf6e32016
Fazel <ahmadfazelpaknehad071@gmail.com>
2025-09-22 20:21:16 +0530

    49645

---
+++ home/serializers.py
@@ -0,0 +1,7 @@
+from res_framework import serializers
+from .models import MenuCategory
+
+class MenuCategorySerializer(serializers,ModelSerializer):
+    class Meta:
+        model = MenuCategory
+        fields = ['id', 'name']
