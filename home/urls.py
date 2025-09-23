5ba2bc398769946568f8e9e79d2b3ee0cd59fff3
Fazel <ahmadfazelpaknehad071@gmail.com>
2025-09-23 20:57:44 +0530

    50803

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
 
+menu_item_list = MenuItemViewSet.as_view({'get': 'list'})
+
 urlpatterns = [
     path('menu-categories/', MenuCategoryListView.as_view(), name='menu-categories'),
+    path('menu-items/', menu_item_list(), name='menu-items-list'),
     
 ]
