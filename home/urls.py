965529d2496097d3e38f6f996bbbb3ed5fb54bf7
Fazel <ahmadfazelpaknehad071@gmail.com>
2025-09-23 21:05:44 +0530

    50803

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ menu_item_list = MenuItemViewSet.as_view({'get': 'list'})
 
 urlpatterns = [
     path('menu-categories/', MenuCategoryListView.as_view(), name='menu-categories'),
-    path('menu-items/', menu_item_list(), name='menu-items-list'),
+    path('menu-items/', menu_item_list, name='menu-items-list'),
     
 ]
