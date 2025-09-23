01a46192ab56c6c3e9e8d2eeefb7ba9b2b54ea3d
Fazel <ahmadfazelpaknehad071@gmail.com>
2025-09-23 21:13:37 +0530

    50803

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
+from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 from .models import MenuCategory
 from .models import MenuItem 
@@ -7,22 +8,23 @@ from .serializers import MenuCategorySerializer
 from .serializers import MenuItemSerializer
 
 class MenuCategoryListView(ListAPIView):
-    queryset = MenuCategory.object.all()
+    queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
 
-class page_size =10
-page_size_query_param ='page_size'
-max_page_size =100
+class MenuItemPagination(PageNumberPagination):
+    page_size =10
+    page_size_query_param ='page_size'
+    max_page_size =100
 
 class MenuItemViewSet(viewsets.ViewSet):
     pagination_class =MenuItemPagination
 
     def list(self, request):
-        query = request.query_param.get('search',None)
+        query = request.query_params.get('search',None)
         items = MenuItem.objects.all()
 
         if query:
-            items =items.filter(name_icontains = query)
+            items = items.filter(name__icontains=query)
 
         paginator = self.pagination_class()
         paginated_items = paginator.paginate_queryset(items,request)
