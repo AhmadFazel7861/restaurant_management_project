5db74c77d543efb3df0960bde6e200725fd3fd75
Fazel <ahmadfazelpaknehad071@gmail.com>
2025-09-23 20:42:53 +0530

    50803

--- home/views.py
+++ home/views.py
@@ -6,6 +6,5 @@ from .models import MenuCategory
 from .serializers import MenuCategorySerializer
 
 class MenuCategoryListView(ListAPIView):
-    queryset = MenuCategory.objects.all()
-
+    queryset = MenuCategory.object.all()
     serializer_class = MenuCategorySerializer
