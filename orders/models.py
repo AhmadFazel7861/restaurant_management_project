115a4329fc34e50af5770f5f91ba77ea45923257
Fazel <ahmadfazelpaknehad071@gmail.com>
2025-09-26 01:48:32 +0530

    50807

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ class OrderStatus(models.Model):
         return self.name
 
 class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     customer_name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
