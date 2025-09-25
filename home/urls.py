bfaf7741fc80b4b1fd1f72443871b222540d9651
Fazel <ahmadfazelpaknehad071@gmail.com>
2025-09-25 20:53:04 +0530

    50804

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import *
 
 menu_item_list = MenuItemViewSet.as_view({'get': 'list'})
+menu_item_update = MenuItemViewSet.as_view({'put': 'update', 'patch': 'partial_update'})
 
 urlpatterns = [
     path('menu-categories/', MenuCategoryListView.as_view(), name='menu-categories'),
     path('menu-items/', menu_item_list, name='menu-items-list'),
+    path('menu-items/<int:pk>/', menu_item_update, name='menu-items-update'),
     
 ]
