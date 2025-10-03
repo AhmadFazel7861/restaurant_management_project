925d663d2939412329f1a66c38eb0424a8e7021a
Fazel <ahmadfazelpaknehad071@gmail.com>
2025-10-04 02:44:47 +0530

    52752

--- orders/models.py
+++ orders/models.py
@@ -40,10 +40,10 @@ class OrderItem(models.Model):
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
     discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
-    is_active = models.models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     valid_until = models.DateField()
 
-    def__str__(self):
+    def __str__ (self):
         return f"{self.code} - {self.discount_percentage}%"
 
         
